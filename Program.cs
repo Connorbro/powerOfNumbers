5414ef2f01329c5c1137e22c237fcebd1130d0db
connor <vbrookesy@hotmail.co.uk>
2020-05-14 15:33:21 +0100

    Initial check-in of module ForProj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
